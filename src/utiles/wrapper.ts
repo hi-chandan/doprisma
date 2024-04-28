2534ebadef83e56484017f1e4b4d80615412c275
LEO <hick317@gmail.com>
2024-04-28 10:24:00 +0530

    fix wrapper

--- src/utiles/wrapper.ts
+++ src/utiles/wrapper.ts
@@ -11,10 +11,8 @@ export const wrapper = (func: ReqHandler): ReqHandler => {
   return (req, res, next) => {
     try {
       const result = func(req, res, next);
-<<<<<<< HEAD
-=======
 
->>>>>>> 01e9a1c70a3e400f3647ee66bef6c1a137f5d7b8
+
       if (result instanceof Promise) {
         result.then((value: any) => handleResult(value, res)).catch(next);
       } else {
@@ -27,12 +25,11 @@ export const wrapper = (func: ReqHandler): ReqHandler => {
 };
 
 function handleResult(result: any, res: any): void {
-<<<<<<< HEAD
+
   if (HttpRes.isHttpRes(result)) {
     res.status(result.status).json(result);
   } else if (result && result !== res) res.send(result);
-=======
+
   if (HttpRes.isHttpRes(result)) res.status(result.status).json(result);
   else if (result && result !== res) res.send(result);
->>>>>>> 01e9a1c70a3e400f3647ee66bef6c1a137f5d7b8
 }
